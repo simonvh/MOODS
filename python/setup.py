c471bf0ee23bff693c172ff241e70a42b4ab3a83
Simon van Heeringen <simon.vanheeringen@gmail.com>
2016-03-23 08:32:18 +0100

    added files from the MOODS-python-1.9.0.tar.gz tarball to make the build work

--- python/setup.py
+++ python/setup.py
@@ -8,29 +8,29 @@ from distutils.core import setup, Extension
 
 
 tools_mod = Extension('MOODS._tools',
-                           sources=['core/tools_wrap.cxx', 'core/moods_tools.cpp','core/moods_misc.cpp'],
-                           include_dirs=["core/"],
+                           sources=['../core/tools_wrap.cxx', '../core/moods_tools.cpp','../core/moods_misc.cpp'],
+                           include_dirs=["../core/"],
                            extra_compile_args=['-march=native', '-O3', '-fPIC', '--std=c++0x'],
                            )
 
 scan_mod = Extension('MOODS._scan',
-                           sources=['core/scan_wrap.cxx',
-                                    'core/moods_scan.cpp',
-                                    'core/motif_0.cpp',
-                                    'core/motif_h.cpp',
-                                    'core/moods_misc.cpp',
-                                    'core/scanner.cpp',
-                                    'core/moods_tools.cpp'],
-                           include_dirs=["core/"],
+                           sources=['../core/scan_wrap.cxx',
+                                    '../core/moods_scan.cpp',
+                                    '../core/motif_0.cpp',
+                                    '../core/motif_h.cpp',
+                                    '../core/moods_misc.cpp',
+                                    '../core/scanner.cpp',
+                                    '../core/moods_tools.cpp'],
+                           include_dirs=["../core/"],
                            extra_compile_args=['-march=native', '-O3', '-fPIC', '--std=c++0x'],
                            )
 
 parsers_mod = Extension('MOODS._parsers',
-                           sources=['core/parsers_wrap.cxx',
-                                    'core/moods_parsers.cpp',
-                                    'core/moods_misc.cpp',
-                                    'core/moods_tools.cpp'],
-                           include_dirs=["core/"],
+                           sources=['../core/parsers_wrap.cxx',
+                                    '../core/moods_parsers.cpp',
+                                    '../core/moods_misc.cpp',
+                                    '../core/moods_tools.cpp'],
+                           include_dirs=["../core/"],
                            extra_compile_args=['-march=native', '-O3', '-fPIC', '--std=c++0x'],
                            )
 
@@ -38,5 +38,5 @@ setup (name = 'MOODS',
        version = '1.9.0-20151120',
        description = 'MOODS: Motif Occurrence Detection Suite',
        ext_modules = [tools_mod, scan_mod, parsers_mod],
-       py_modules = ["MOODS.tools", "MOODS.scan, MOODS.parsers"],
+       py_modules = ["MOODS.tools", "MOODS.scan", "MOODS.parsers"],
 )
