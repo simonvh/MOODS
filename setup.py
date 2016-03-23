328342c03de64e077d544d4f584b37dec5d78a92
simonvh <simon.vanheeringen@gmail.com>
2016-03-23 12:10:44 +0100

    moved things around

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for MOODS
+"""
+
+from distutils.core import setup, Extension
+
+
+tools_mod = Extension('MOODS._tools',
+                           sources=['core/tools_wrap.cxx', 'core/moods_tools.cpp','core/moods_misc.cpp'],
+                           include_dirs=["core/"],
+                           extra_compile_args=['-march=native', '-O3', '-fPIC', '--std=c++0x'],
+                           )
+
+scan_mod = Extension('MOODS._scan',
+                           sources=['core/scan_wrap.cxx',
+                                    'core/moods_scan.cpp',
+                                    'core/motif_0.cpp',
+                                    'core/motif_h.cpp',
+                                    'core/moods_misc.cpp',
+                                    'core/scanner.cpp',
+                                    'core/moods_tools.cpp'],
+                           include_dirs=["core/"],
+                           extra_compile_args=['-march=native', '-O3', '-fPIC', '--std=c++0x'],
+                           )
+
+parsers_mod = Extension('MOODS._parsers',
+                           sources=['core/parsers_wrap.cxx',
+                                    'core/moods_parsers.cpp',
+                                    'core/moods_misc.cpp',
+                                    'core/moods_tools.cpp'],
+                           include_dirs=["core/"],
+                           extra_compile_args=['-march=native', '-O3', '-fPIC', '--std=c++0x'],
+                           )
+
+setup (name = 'MOODS',
+       version = '1.9.0-20151120',
+       description = 'MOODS: Motif Occurrence Detection Suite',
+       ext_modules = [tools_mod, scan_mod, parsers_mod],
+       py_modules = ["MOODS.tools", "MOODS.scan", "MOODS.parsers"],
+)
